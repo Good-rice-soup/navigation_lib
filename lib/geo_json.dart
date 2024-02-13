55f3acce28a47d7610ac8a68b5251d4ec4265d15
ScreamingPossum <varnachevlev@gmail.com>
2024-02-14 01:47:21 +0400

    feat: added new files

---
+++ lib/geo_json.dart
@@ -0,0 +1,7 @@
+
+class GeoJson {
+  Future<void> getGeohashFromLatLong(double lat, double long) async {
+    //var geohash = GeoHasher.encode(lat, long);
+    //print(geohash);
+  }
+}
--- lib/geo_utils.dart
+++ lib/geo_utils.dart
@@ -1,3 +1,6 @@
 library geo_utils;
 
+export 'package:geo_utils/geo_hasher.dart';
+export 'package:geo_utils/geo_json.dart';
+
 export 'geo_math.dart';

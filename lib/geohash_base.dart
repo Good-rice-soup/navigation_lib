7d6a8719b419403210b69c8b5cfb77d0ed62b72d
Viktor Karpov <viktor.softprog@gmail.com>
2024-02-13 17:05:29 +0400

    feat:add Geohash

--- lib/geohash_base.dart
+++ lib/geohash_base.dart
@@ -1,8 +1,3 @@
-// Copyright (c) 2015-2018, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
-library geohash.base;
-
 import 'dart:math';
 
 /// A collection of static functions to work with geohashes, as exlpained

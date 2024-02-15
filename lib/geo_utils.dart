ce186fd6f5ed087d18cd448b46d0c28da7275cb6
ScreamingPossum <varnachevlev@gmail.com>
2024-02-15 15:31:36 +0400

    refactor: deleted unused files, other prepared for refactoring.

--- lib/geo_utils.dart
+++ lib/geo_utils.dart
@@ -1,8 +1,5 @@
 library geo_utils;
 
-export 'package:geo_utils/geo_hasher.dart';
-export 'package:geo_utils/geo_json.dart';
-
 export 'geo_math.dart';
 export 'geohash_base.dart';
 export 'geohash_utils.dart';
--- lib/geohash_utils.dart
+++ lib/geohash_utils.dart
@@ -6,8 +6,7 @@ import 'geohash_base.dart';
 
 //ignore_for_file: avoid_classes_with_only_static_members
 class GeohashUtils{
- static List<String> getWayGeohashes(
-      {required List<gm.LatLng> points, required int precision}) {
+ static List<String> getWayGeohashes({required List<LatLng> points, required int precision}) {
     final Set<String> geohashes = {};
 
     for (int i = 0; i < points.length - 1; i++) {
@@ -17,26 +16,22 @@ class GeohashUtils{
       //final LatLng end = points[i + 1];
 
       // Добавляем геохеш начальной точки
-      geohashes.add(
-          Geohash.encode(start.latitude, start.longitude, codeLength: precision));
+      geohashes.add(Geohash.encode(start.latitude, start.longitude, codeLength: precision));
 
       final double distance = const Distance().as(LengthUnit.Meter, start, end);
-      /* final double step = Geohash.distance(
-        precision); // Расстояние, соответствующее одной плитке геохеша на этой точности
-*/
+      /*
+      final double step = Geohash.distance(precision); // Расстояние, соответствующее одной плитке геохеша на этой точности
+      */
+
       //TODO
       const double step = 5000;
       for (double d = step; d < distance; d += step) {
-        final LatLng intermediate = const Distance()
-            .offset(start, d, const Distance().bearing(start, end));
-        geohashes.add(Geohash.encode(
-            intermediate.latitude, intermediate.longitude,
-            codeLength: precision));
+        final LatLng intermediate = const Distance().offset(start, d, const Distance().bearing(start, end));
+        geohashes.add(Geohash.encode(intermediate.latitude, intermediate.longitude, codeLength: precision));
       }
 
       // Добавляем геохеш конечной точки
-      geohashes.add(
-          Geohash.encode(end.latitude, end.longitude, codeLength: precision));
+      geohashes.add(Geohash.encode(end.latitude, end.longitude, codeLength: precision));
     }
 
     return geohashes.toList();

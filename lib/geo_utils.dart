ce80b0c90cf366ef344576171a048321bee430e3
Good-rice-soup <varnachevlev@gmail.com>
2024-10-08 10:45:09 +0400

    refactor: file name rename

--- lib/geo_utils.dart
+++ lib/geo_utils.dart
@@ -3,6 +3,6 @@ library geo_utils;
 export 'geo_calculation_aggregator.dart';
 export 'geo_hash_utils.dart';
 export 'geo_math.dart';
-export 'polyline_helper_config.dart';
+export 'polylines_simplifier.dart';
 export 'polyline_util.dart';
 export 'route_manager.dart';

94f1c98bf2cb79550d65c793550c01b02c80b79e
Good-rice-soup <varnachevlev@gmail.com>
2024-10-15 09:24:30 +0400

    refactor: adding new exports

--- lib/geo_utils.dart
+++ lib/geo_utils.dart
@@ -1,8 +1,10 @@
 library geo_utils;
 
+export 'config_classes.dart';
 export 'geo_calculation_aggregator.dart';
 export 'geo_hash_utils.dart';
 export 'geo_math.dart';
+export 'interpolation_util.dart';
 export 'polyline_util.dart';
 export 'polylines_simplifier.dart';
 export 'route_cutter.dart';

d8c69572debe487310f0a120830611d1fff108f7
Good-rice-soup <varnachevlev@gmail.com>
2024-12-04 18:48:52 +0400

    refactor: removed old peaces of code

--- lib/polylines_simplifier.dart
+++ lib/polylines_simplifier.dart
@@ -122,148 +122,6 @@ class PolylineSimplifier {
     originalRouteRouteManager.updateCurrentLocation(currentLocation);
   }
 
-  @Deprecated('Use [getRoute]')
-  List<LatLng> getRoute3({
-    required LatLngBounds bounds,
-    required int zoom,
-    LatLng? currentLocation,
-    int replaceByOriginalRouteIfLessThan = 200,
-  }) {
-    final ZoomToFactor currentZoomConfig = config.getConfigForZoom(zoom);
-    final double tolerance = currentZoomConfig.routeSimplificationFactor;
-    final List<LatLng> currentZoomRoute = _zoomToManager[zoom]!.route;
-    final LatLngBounds expandedBounds =
-        expandBounds(bounds, currentZoomConfig.boundsExpansionFactor);
-
-    if (currentZoomRoute.isEmpty) return [];
-
-    if (currentZoomConfig.isUseOriginalRouteInVisibleArea) {
-      final List<LatLng> detailedRoute =
-          _detailRoute(currentZoomRoute, expandedBounds, tolerance);
-      if (currentLocation != null) {
-        final List<LatLng> cuttedDetailedRoute = [currentLocation];
-        _updateRouteManagers(currentLocation: currentLocation);
-
-        final int originalRouteNextRoutePointIndex =
-            originalRouteRouteManager.nextRoutePointIndex;
-
-        final int amountOfPointsToFinish =
-            _route.length - originalRouteNextRoutePointIndex;
-        if (amountOfPointsToFinish <= replaceByOriginalRouteIfLessThan) {
-          cuttedDetailedRoute
-              .addAll(_route.sublist(originalRouteNextRoutePointIndex));
-          return cuttedDetailedRoute;
-        }
-
-        if (expandedBounds.contains(currentLocation)) {
-          final LatLng originalRouteNextRoutePoint =
-              originalRouteRouteManager.nextRoutePoint;
-          final int index = detailedRoute.indexOf(originalRouteNextRoutePoint);
-          cuttedDetailedRoute.addAll(detailedRoute.sublist(index));
-          //TODO: придумать что-то получше, чем indexOf()
-        } else {
-          final int currentZoomNextRoutePointIndex =
-              _zoomToManager[zoom]!.nextRoutePointIndex;
-          cuttedDetailedRoute.addAll(detailedRoute.sublist(
-              currentZoomNextRoutePointIndex)); //////////////////////////////////////////////////////
-          //индекс следующей точки передаётся не в путь зумроута, а в ДЕТАЛИЗИРОВАННЫЙ путь
-        }
-
-        return cuttedDetailedRoute;
-      }
-      return detailedRoute;
-    }
-
-    if (currentLocation != null) {
-      final List<LatLng> cuttedCurrentZoomRoute = [currentLocation];
-      _updateRouteManagers(currentLocation: currentLocation);
-
-      final LatLng originalRouteNextRoutePoint =
-          originalRouteRouteManager.nextRoutePoint;
-      if (originalRouteNextRoutePoint == currentLocation) {
-        return cuttedCurrentZoomRoute;
-      }
-
-      final int currentZoomNextRoutePointIndex =
-          _zoomToManager[zoom]!.nextRoutePointIndex;
-      cuttedCurrentZoomRoute
-          .addAll(currentZoomRoute.sublist(currentZoomNextRoutePointIndex));
-      return cuttedCurrentZoomRoute;
-    }
-    return currentZoomRoute;
-  }
-
-  List<LatLng> _detailRoute(
-    List<LatLng> zoomRoute,
-    LatLngBounds bounds,
-    double tolerance,
-  ) {
-    final Map<int, int> mapping = _toleranceToMappedZoomRoutes[tolerance]!;
-    final List<LatLng> resultPath = [];
-    bool insideBounds = false;
-    //содержит пары входа и выхода из области видимости
-    // проверяется по четности нечетности количества элементов в списке
-    final List<int> listOfReplacements = [];
-
-    int i = 0;
-    for (final LatLng point in zoomRoute) {
-      if (bounds.contains(point)) {
-        if (insideBounds == false) listOfReplacements.add(i);
-        insideBounds = true;
-      } else {
-        if (insideBounds == true) listOfReplacements.add(i);
-        insideBounds = false;
-      }
-      i++;
-    }
-
-    //на случай если конец пути покрыт зоной видимости, предыдущий цикл не
-    // закроет пару замены пути. но при этом надо сделать проверку на дубликаты
-    if (listOfReplacements.length.isOdd) {
-      listOfReplacements.add(zoomRoute.length - 1);
-    }
-
-    if (listOfReplacements.isEmpty) return zoomRoute;
-
-    resultPath.addAll(zoomRoute.sublist(0, listOfReplacements[0]));
-    for (int i = 0; i < (listOfReplacements.length - 1); i += 2) {
-      final int startPointIndex = listOfReplacements[i];
-      final int endPointIndex = listOfReplacements[i + 1];
-      final int startPointIndexInOriginalRoute = mapping[startPointIndex]!;
-      final int endPointIndexInOriginalRoute = mapping[endPointIndex]!;
-      final List<LatLng> detailedRoutePart = _route.sublist(
-          startPointIndexInOriginalRoute, endPointIndexInOriginalRoute);
-      if (resultPath.isEmpty) {}
-      resultPath.addAll(detailedRoutePart);
-
-      if (i + 1 < listOfReplacements.length - 1) {
-        final List<LatLng> intermediateRoutePart =
-            zoomRoute.sublist(endPointIndex, listOfReplacements[i + 2]);
-        resultPath.addAll(intermediateRoutePart);
-      }
-    }
-
-    final List<LatLng> lastRoutePart =
-        zoomRoute.sublist(listOfReplacements.last);
-    resultPath.addAll(lastRoutePart);
-    if (resultPath.last == resultPath[resultPath.length - 2]) {
-      resultPath.removeAt(resultPath.length - 1);
-    }
-    return resultPath;
-  }
-
-  static List<LatLng> interpolatePoints(LatLng p1, LatLng p2, int numPoints) {
-    final List<LatLng> interpolatedPoints = [];
-    for (int i = 1; i <= numPoints; i++) {
-      final double fraction = i / (numPoints + 1);
-      final double lat = p1.latitude + (p2.latitude - p1.latitude) * fraction;
-      final double lng =
-          p1.longitude + (p2.longitude - p1.longitude) * fraction;
-      interpolatedPoints.add(LatLng(lat, lng));
-    }
-    return interpolatedPoints;
-  }
-
   LatLngBounds expandBounds(LatLngBounds bounds, double factor) {
     final double lat =
         (bounds.northeast.latitude - bounds.southwest.latitude).abs();
@@ -313,7 +171,7 @@ class PolylineSimplifier {
 
     //detailing stage
     if (needReplace) {
-      resultRoute = _detailRoute1(
+      resultRoute = _detailRoute(
         resultRoute,
         expandedBounds,
         tolerance,
@@ -324,7 +182,26 @@ class PolylineSimplifier {
     return resultRoute;
   }
 
-  List<LatLng> _detailRoute1(
+  List<int> _segmentConnector(List<int> list) {
+    final List<int> newList = [list.first];
+    for (int i = 1; i < list.length - 1; i += 2) {
+      final int a = list[i];
+      final int b = list[i + 1];
+      //b > a always
+      if (b - a != 1) {
+        newList
+          ..add(a)
+          ..add(b);
+      } else {
+        print('[GeoUtils:RouteSimplifier] removed closing point: $a');
+        print('[GeoUtils:RouteSimplifier] removed opening point: $b');
+      }
+    }
+    newList.add(list.last);
+    return newList;
+  }
+
+  List<LatLng> _detailRoute(
     List<LatLng> route,
     LatLngBounds bounds,
     double tolerance,
@@ -382,22 +259,15 @@ class PolylineSimplifier {
     return resultPath;
   }
 
-  List<int> _segmentConnector(List<int> list) {
-    final List<int> newList = [list.first];
-    for (int i = 1; i < list.length - 1; i += 2) {
-      final int a = list[i];
-      final int b = list[i + 1];
-      //b > a always
-      if (b - a != 1) {
-        newList
-          ..add(a)
-          ..add(b);
-      } else {
-        print('[GeoUtils:RouteSimplifier] removed closing point: $a');
-        print('[GeoUtils:RouteSimplifier] removed opening point: $b');
-      }
+  static List<LatLng> interpolatePoints(LatLng p1, LatLng p2, int numPoints) {
+    final List<LatLng> interpolatedPoints = [];
+    for (int i = 1; i <= numPoints; i++) {
+      final double fraction = i / (numPoints + 1);
+      final double lat = p1.latitude + (p2.latitude - p1.latitude) * fraction;
+      final double lng =
+          p1.longitude + (p2.longitude - p1.longitude) * fraction;
+      interpolatedPoints.add(LatLng(lat, lng));
     }
-    newList.add(list.last);
-    return newList;
+    return interpolatedPoints;
   }
 }

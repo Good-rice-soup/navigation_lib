8229bf1240e9c70d31396213b0c38aece142668b
Good-rice-soup <varnachevlev@gmail.com>
2024-12-27 19:10:03 +0400

    feat: crash fix guess

--- lib/new_route_manager.dart
+++ lib/new_route_manager.dart
@@ -904,7 +904,7 @@ class NewRouteManager {
   void deleteSidePoint(LatLng point) {
     final int index = _alignedSidePoints.indexOf(point);
     _sidePointsStatesHashTable.remove(index);
-    _sidePointsData.remove(_sidePointsData.firstWhere((e) => e.$1 == index));
+    //_sidePointsData.remove(_sidePointsData.firstWhere((e) => e.$1 == index));
     _alignedSidePoints.remove(point);
   }
 

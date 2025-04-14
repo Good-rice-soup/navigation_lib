fa17c19c1bf864ef53f03e5dbdf56ace78d80ccf
Good-rice-soup <varnachevlev@gmail.com>
2025-04-14 12:01:45 +0400

    refactor: removing geo_calculation_aggregator.dart, it's tests and export

--- lib/geo_utils.dart
+++ lib/geo_utils.dart
@@ -1,7 +1,6 @@
 library geo_utils;
 
 export 'config_classes.dart';
-export 'geo_calculation_aggregator.dart';
 export 'geo_hash_utils.dart';
 export 'geo_math.dart';
 export 'interpolation_util.dart';

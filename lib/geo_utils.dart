e3c26cf39d85b47430a960763133e32a72119ab8
Good-rice-soup <varnachevlev@gmail.com>
2025-04-17 00:14:15 +0400

    refactor: adding expandBounds func

--- lib/geo_utils.dart
+++ lib/geo_utils.dart
@@ -53,3 +53,20 @@ double metersToLatDegrees(double meters) {
 double metersToLngDegrees(double meters, double latitude) {
   return meters / (metersPerDegree * cos(toRadians(latitude)));
 }
+
+LatLngBounds expandBounds(LatLngBounds bounds, double factor) {
+final double lat =
+(bounds.northeast.latitude - bounds.southwest.latitude).abs();
+final double lng =
+(bounds.northeast.longitude - bounds.southwest.longitude).abs();
+final LatLng southwest = LatLng(
+  bounds.southwest.latitude - (lat * (factor - 1) / 2),
+  bounds.southwest.longitude - (lng * (factor - 1) / 2),
+);
+final LatLng northeast = LatLng(
+  bounds.northeast.latitude + (lat * (factor - 1) / 2),
+  bounds.northeast.longitude + (lng * (factor - 1) / 2),
+);
+
+return LatLngBounds(southwest: southwest, northeast: northeast);
+}

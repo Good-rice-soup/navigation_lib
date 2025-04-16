552919062d35159f8266d4e164af87d76c173718
Good-rice-soup <varnachevlev@gmail.com>
2025-04-16 23:23:33 +0400

    refactor: rename the library like it`s repository

---
+++ lib/geo_math_lib.dart
@@ -0,0 +1,10 @@
+library geo_math_lib;
+
+export 'config_classes.dart';
+export 'geo_hash_utils.dart';
+export 'geo_utils.dart';
+export 'interpolation_util.dart';
+export 'navigation_utils.dart';
+export 'polyline_util.dart';
+export 'polylines_simplifier.dart';
+export 'route_manager.dart';

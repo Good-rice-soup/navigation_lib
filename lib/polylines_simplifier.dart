b68d7c59e82a78379ecafd64afd8dc15e930d1f1
Good-rice-soup <varnachevlev@gmail.com>
2025-05-16 04:47:45 +0400

    refactor: simplifying the code

--- lib/polylines_simplifier.dart
+++ lib/polylines_simplifier.dart
@@ -31,6 +31,34 @@ zoom level	tile side size at equator
 21	        19 m
 */
 
+class _ManagerConfig {
+  const _ManagerConfig({
+    required this.searchRectWidth,
+    required this.searchRectExtension,
+    required this.finishLineDist,
+    required this.lengthOfLists,
+    required this.additionalChecksDist,
+    required this.maxVectDeviationInDeg,
+  });
+
+  final double searchRectWidth;
+  final double searchRectExtension;
+  final double finishLineDist;
+  final int lengthOfLists;
+  final double additionalChecksDist;
+  final double maxVectDeviationInDeg;
+
+  RouteManagerCore createManager(List<LatLng> route) {
+    return RouteManagerCore(
+        route: route,
+        searchRectWidth: searchRectWidth,
+        searchRectExtension: searchRectExtension,
+        finishLineDist: finishLineDist,
+        additionalChecksDist: additionalChecksDist,
+        maxVectDeviationInDeg: maxVectDeviationInDeg);
+  }
+}
+
 class PolylineSimplifier {
   PolylineSimplifier({
     required List<LatLng> route,
@@ -43,9 +71,7 @@ class PolylineSimplifier {
     double maxVectDeviationInDeg = 45,
   }) {
     _routeConfig = RouteSimplificationConfig(routeConfig);
-
-    _origRouteRM = RouteManagerCore(
-        route: route,
+    final _ManagerConfig mConfig = _ManagerConfig(
         searchRectWidth: searchRectWidth,
         searchRectExtension: searchRectExtension,
         finishLineDist: finishLineDist,
@@ -53,16 +79,9 @@ class PolylineSimplifier {
         additionalChecksDist: additionalChecksDist,
         maxVectDeviationInDeg: maxVectDeviationInDeg);
 
+    _origRouteRM = mConfig.createManager(route);
+    _shiftedRM = mConfig.createManager(route);
     _route = _origRouteRM.route;
-
-    _shiftedRM = RouteManagerCore(
-        route: route,
-        searchRectWidth: searchRectWidth,
-        searchRectExtension: searchRectExtension,
-        finishLineDist: finishLineDist,
-        lengthOfLists: lengthOfLists,
-        additionalChecksDist: additionalChecksDist,
-        maxVectDeviationInDeg: maxVectDeviationInDeg);
 
     final Map<double, Set<int>> toleranceGroups = {};
     for (final ZoomConfig config in _routeConfig.zoomConfigs.values) {
@@ -72,8 +91,8 @@ class PolylineSimplifier {
     }
 
     for (final MapEntry<double, Set<int>> entry in toleranceGroups.entries) {
-      final tolerance = entry.key;
-      final zooms = entry.value;
+      final double tolerance = entry.key;
+      final Set<int> zooms = entry.value;
 
       final Map<int, int> simplifiedToOriginal = {};
       final List<LatLng> simplifiedRoute =
@@ -83,19 +102,10 @@ class PolylineSimplifier {
       _originalToSimplifiedMap[tolerance] = simplifiedToOriginal
           .map((simpInd, origInd) => MapEntry(origInd, simpInd));
 
-      final RouteManagerCore manager = RouteManagerCore(
-          route: simplifiedRoute,
-          searchRectWidth: searchRectWidth,
-          searchRectExtension: searchRectExtension,
-          finishLineDist: finishLineDist,
-          lengthOfLists: lengthOfLists,
-          additionalChecksDist: additionalChecksDist,
-          maxVectDeviationInDeg: maxVectDeviationInDeg);
-
-      for (final int zoom in zooms) {
-        _zoomToManager[zoom] = manager;
-      }
+      final RouteManagerCore manager = mConfig.createManager(simplifiedRoute);
+      zooms.forEach((zoom) => _zoomToManager[zoom] = manager);
     }
+    _managersSet.addAll(_zoomToManager.values);
   }
 
   List<LatLng> _route = [];
@@ -105,115 +115,56 @@ class PolylineSimplifier {
   final Map<double, Map<int, int>> _simplifiedToOriginalMap = {};
   final Map<double, Map<int, int>> _originalToSimplifiedMap = {};
   final Map<int, RouteManagerCore> _zoomToManager = {};
-
-  List<int> _segmentConnector(List<int> list) {
-    final List<int> newList = [list.first];
-    for (int i = 1; i < list.length - 1; i += 2) {
-      final int a = list[i];
-      final int b = list[i + 1];
-      //b > a always
-      if (b - a != 1) {
-        newList
-          ..add(a)
-          ..add(b);
-      }
-    }
-    newList.add(list.last);
-    return newList;
-  }
+  final Set<RouteManagerCore> _managersSet = {};
 
   void _updateRouteManagers(LatLng currLoc, [int? curLocInd]) {
-    final Iterable<int> keys = _zoomToManager.keys;
-    for (final int key in keys) {
-      _zoomToManager[key]!.updateCurrentLocation(currLoc, curLocInd);
-    }
-
+    _managersSet.forEach((e) => e.updateCurrentLocation(currLoc, curLocInd));
     _origRouteRM.updateCurrentLocation(currLoc, curLocInd);
   }
 
-  LatLng _getPointProjection(LatLng currLoc, LatLng start, LatLng end) {
-    final double dLat = end.latitude - start.latitude;
-    final double dLng = end.longitude - start.longitude;
-
-    if (dLat == 0 && dLng == 0) throw ArgumentError('Start and end are same');
-
-    // start(x1, y1), end(x2, y2), point(x0, y0)
-    final double x0 = currLoc.latitude;
-    final double x1 = start.latitude;
-    final double y0 = currLoc.longitude;
-    final double y1 = start.longitude;
-
-    if (dLng == 0 && dLat != 0) return LatLng(x0, y1);
-    if (dLng != 0 && dLat == 0) return LatLng(x1, y0);
-
-    // coefficients in line equations system Ax + By + C = 0
-    // A = y2 - y1, B = x2 - x1
-    final double aa = dLng * dLng; // A^2
-    final double bb = dLat * dLat; // B^2
-    final double ab = dLng * dLat;
-    final double denominator = aa + bb;
-
-    final double y = (bb * y1 + ab * (x0 - x1) + aa * y0) / denominator;
-    final double x = (bb * y1 + ab * (x0 - x1) + aa * y0) / denominator;
-
-    return LatLng(x, y);
-  }
-
-  List<LatLng> getRoute(
-    LatLngBounds bounds,
-    int zoom, [
-    LatLng? currLoc,
+  int _cutRoute(
+    LatLng currLoc,
     int? nextPointInd,
-  ]) {
-    final ZoomConfig zoomConfig = _routeConfig.getConfig(zoom);
-    final LatLngBounds expandedBounds =
-        expandBounds(bounds, zoomConfig.boundsExpansion);
-    final double tolerance = zoomConfig.simplificationTolerance;
-    final RouteManagerCore manager = _zoomToManager[zoom]!;
-    final bool useOriginalRoute = zoomConfig.useOriginalRouteInView;
+    bool useOriginalRoute,
+    double tolerance,
+    RouteManagerCore manager,
+    List<LatLng> resultRoute,
+  ) {
     int startingPointIndex = 0;
-    List<LatLng> resultRoute = [];
+    _updateRouteManagers(
+        currLoc, nextPointInd == null ? nextPointInd : nextPointInd - 1);
 
-    if (currLoc != null) {
-      final bool indIsNull = nextPointInd == null;
-      _updateRouteManagers(
-          currLoc, indIsNull ? nextPointInd : nextPointInd - 1);
-
-      if (nextPointInd != null) {
-        if (nextPointInd >= _route.length || nextPointInd <= 0) {
-          throw ArgumentError('nextPointIndex out of range: $nextPointInd');
-        }
-
-        final Map<int, int> indexes = _originalToSimplifiedMap[tolerance]!;
-        final List<int> sortedKeys = indexes.keys.toList()..sort();
-        final int index =
-            indexes[sortedKeys.firstWhere((k) => k >= nextPointInd)]!;
-
-        startingPointIndex = useOriginalRoute ? index - 1 : index;
-      } else {
-        startingPointIndex = useOriginalRoute
-            ? manager.nextRoutePointIndex - 1
-            : manager.nextRoutePointIndex;
+    if (nextPointInd != null) {
+      if (nextPointInd >= _route.length || nextPointInd <= 0) {
+        throw ArgumentError('nextPointIndex out of range: $nextPointInd');
       }
 
-      if (!useOriginalRoute) resultRoute.add(currLoc);
-      resultRoute.addAll(manager.route.sublist(startingPointIndex));
+      final Map<int, int> indexes = _originalToSimplifiedMap[tolerance]!;
+      final List<int> sortedKeys = indexes.keys.toList()..sort();
+      final int ind = indexes[sortedKeys.firstWhere((k) => k >= nextPointInd)]!;
+
+      startingPointIndex = useOriginalRoute ? ind - 1 : ind;
     } else {
-      resultRoute = manager.route;
+      startingPointIndex = useOriginalRoute
+          ? manager.nextRoutePointIndex - 1
+          : manager.nextRoutePointIndex;
     }
 
-    if (useOriginalRoute) {
-      resultRoute = _detailRoute(
-        resultRoute,
-        expandedBounds,
-        tolerance,
-        startingPointIndex,
-        currLoc,
-        nextPointInd,
-      );
-    }
+    if (!useOriginalRoute) resultRoute.add(currLoc);
+    resultRoute.addAll(manager.route.sublist(startingPointIndex));
 
-    return resultRoute;
+    return startingPointIndex;
+  }
+
+  // elements are arranged in increasing order
+  List<int> _segmentConnector(List<int> list) {
+    final List<int> newList = [list.first];
+    for (int i = 1; i < list.length - 1; i += 2) {
+      final int a = list[i], b = list[i + 1];
+      if (b - a != 1) newList.addAll([a, b]);
+    }
+    newList.add(list.last);
+    return newList;
   }
 
   List<LatLng> _detailRoute(
@@ -254,7 +205,7 @@ class PolylineSimplifier {
 
       if (i == 0 && !locIsNull) {
         final int nextRPInd = nextPointInd ?? _origRouteRM.nextRoutePointIndex;
-        final LatLng shiftedLoc = _getPointProjection(
+        final LatLng shiftedLoc = getPointProjection(
             currLoc, _route[nextRPInd - 1], _route[nextRPInd]);
         resultPath.add(shiftedLoc);
         _shiftedRM.updateCurrentLocation(
@@ -270,5 +221,38 @@ class PolylineSimplifier {
 
     resultPath.addAll(route.sublist(replacementsList.last));
     return resultPath;
+  }
+
+  List<LatLng> getRoute(
+    LatLngBounds bounds,
+    int zoom, [
+    LatLng? currLoc,
+    int? nextPointInd,
+  ]) {
+    final ZoomConfig zoomConfig = _routeConfig.getConfig(zoom);
+    final double tolerance = zoomConfig.simplificationTolerance;
+    final bool useOriginalRoute = zoomConfig.useOriginalRouteInView;
+    final RouteManagerCore manager = _zoomToManager[zoom]!;
+    int startingPointIndex = 0;
+    List<LatLng> route = [];
+
+    if (currLoc != null) {
+      startingPointIndex = _cutRoute(
+          currLoc, nextPointInd, useOriginalRoute, tolerance, manager, route);
+    } else {
+      route = manager.route;
+    }
+
+    if (useOriginalRoute) {
+      route = _detailRoute(
+        route,
+        expandBounds(bounds, zoomConfig.boundsExpansion),
+        tolerance,
+        startingPointIndex,
+        currLoc,
+        nextPointInd,
+      );
+    }
+    return route;
   }
 }

52529cf0023b180b199312501b3cae68c9112b7f
Good-rice-soup <varnachevlev@gmail.com>
2025-08-15 12:59:45 +0400

    fix: bounds expansion error fix

--- lib/geo_utils.dart
+++ lib/geo_utils.dart
@@ -149,11 +149,11 @@ LatLngBounds expandBounds(LatLngBounds bounds, [double expFactor = 1]) {
       (bounds.northeast.longitude - bounds.southwest.longitude).abs();
   final LatLng southwest = LatLng(
     bounds.southwest.latitude - (lat * (expFactor - 1) / 2),
-    bounds.southwest.longitude - (lng * (expFactor - 1) / 2),
+    (bounds.southwest.longitude - (lng * (expFactor - 1) / 2)).clamp(-180, 180),
   );
   final LatLng northeast = LatLng(
     bounds.northeast.latitude + (lat * (expFactor - 1) / 2),
-    bounds.northeast.longitude + (lng * (expFactor - 1) / 2),
+    (bounds.northeast.longitude + (lng * (expFactor - 1) / 2)).clamp(-180, 180),
   );
 
   return LatLngBounds(southwest: southwest, northeast: northeast);

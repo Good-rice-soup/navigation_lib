35741a67299173df9b0d0652dde6abb1c56898c7
Good-rice-soup <lvarn.dev@gmail.com>
2026-03-17 05:06:16 +0400

    feat: added transfer object types drafts

---
+++ lib/route_transfer_objects.dart
@@ -0,0 +1,58 @@
+import 'dart:typed_data';
+
+import 'new_search_rect.dart';
+import 'side_point.dart';
+
+/// Неизменяемая стартовая конфигурация для инициализации RouteManager.
+/// Передается из DAO в момент создания менеджера.
+class RMConfig {
+  RMConfig({
+    required this.route,
+    required this.distFromStart,
+    required this.segmentsLen,
+    required this.srBuffer,
+    required this.alignedSP,
+    required this.wpIndices,
+    required this.routeLen,
+    required this.vectorHistorySize,
+  });
+
+  // Массивы передаются по ссылке (zero-copy)
+  final Float64List route;
+  final Float64List distFromStart;
+  final Float64List segmentsLen;
+  final SearchRectBuffer srBuffer;
+  final RawSidePointsBuffer alignedSP;
+  final List<int> wpIndices;
+
+  final double routeLen;
+  final int vectorHistorySize;
+}
+
+/// Мутабельное состояние маршрута.
+/// Генерируется менеджером и передается в DAO для сохранения на диск.
+class RMState {
+  RMState({
+    required this.currRPInd,
+    required this.nextRPInd,
+    required this.prevRPInd,
+    required this.currSegmInd,
+    required this.prevSegmInd,
+    required this.nextWPIndex,
+    required this.isOnRoute,
+    required this.isJump,
+    // Если менеджер мутирует сами буферы напрямую, передавать их здесь не обязательно,
+    // но если нужно отслеживать добавление новых точек, можно добавить:
+    // required this.alignedSP,
+  });
+
+  final int currRPInd;
+  final int nextRPInd;
+  final int prevRPInd;
+  final int currSegmInd;
+  final int prevSegmInd;
+  final int nextWPIndex;
+
+  final bool isOnRoute;
+  final bool isJump;
+}
